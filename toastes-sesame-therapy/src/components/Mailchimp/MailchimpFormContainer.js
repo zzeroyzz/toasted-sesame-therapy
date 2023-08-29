4520c401fbdf21735627a99027c41be4a88cd6ec
zzeroyzz <zzeroyzz@gmail.com>
2023-08-29 12:59:27 -0400

    lint

--- toastes-sesame-therapy/src/components/Mailchimp/MailchimpFormContainer.js
+++ toastes-sesame-therapy/src/components/Mailchimp/MailchimpFormContainer.js
@@ -1,36 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 import CustomForm from './CustomForm';
-import { ChakraProvider, Skeleton } from '@chakra-ui/react';
-import { Spinner } from '@chakra-ui/react'
-import ToastedLoading from '../ToastedLoading/ToastedLoading';
+
 
 const MailchimpFormContainer = props => {
-    const [isLoading, setIsLoading] = useState(true);
-const LoadingSkeleton = () => {
-  return (
-    <div className="container">
-      <Skeleton height="20px" mb="1rem" />
-      <Skeleton height="20px" mb="1rem" />
-      <Skeleton height="20px" mb="1rem" />
-      {/* Add more Skeletons for other loading content */}
-    </div>
-  );
-};
-      useEffect(() => {
-    // Simulate loading data or any async operations
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
-
 
 
     const postUrl = `https://gmail.us21.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
     return (
         <div className="mc__form-container">
-            {/* {isLoading ? <ToastedLoading/> : */}
             <MailchimpSubscribe
                 url={postUrl}
                 render={({ subscribe, status, message }) => (
@@ -41,7 +20,6 @@ const LoadingSkeleton = () => {
                     />
                 )}
             />
-            {/* } */}
         </div>
     );
 };
