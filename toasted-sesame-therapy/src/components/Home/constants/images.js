8b61e5c0dbc65870280bbad2f0e0df7f65bb19f5
Kato <zzeroyzz@gmail.com>
2023-12-02 19:33:50 -0500

    feat: updated home component and constants

---
+++ toasted-sesame-therapy/src/components/Home/constants/images.js
@@ -0,0 +1,49 @@
+import CheckOutMyTherapyStyle from '../../../assets/CheckOutMyTherapyStyle.jpg';
+import ExporeMyExpertise from '../../../assets/ExploreMyExpertise.jpg';
+import PeekIntoMyStory from '../../../assets/PeekIntoMyStory.jpg';
+import GetInTouch from '../../../assets/GetInTouch.jpg';
+import LearnMore from '../../../assets/LearnMore.jpg';
+
+export const imagesData = [
+  {
+    image: GetInTouch,
+    header: 'Embrace Growth with Toasted Sesame Therapy',
+    subtext:
+      'Just as toasted sesame seeds release their rich aroma when gently warmed, our therapy practice seeks to unlock the hidden strengths within you.',
+    buttonText: 'Get in Touch',
+    buttonLink: '/contact-us',
+  },
+  {
+    image: CheckOutMyTherapyStyle,
+    header: 'The Key Ingredient, A Culturally Responsive Style',
+    subtext:
+      'My therapy style is all about recognizing and respecting the diverse backgrounds and experiences that shape each person\'s healing journey.',
+    buttonText: 'Check Out My Therapy Style',
+    buttonLink: '/therapy-style',
+  },
+  {
+    image: ExporeMyExpertise,
+    header: 'Personalized Healing Recipes',
+    subtext:
+      'Discover more about my educational expertise, and how a focus on Trauma and Somatic Therapy is the secret sauce to creating your own unique healing space. ',
+    buttonText: 'Explore My Expertise',
+    buttonLink: '/expertise',
+  },
+
+  {
+    image: PeekIntoMyStory,
+    header: 'A Dash of Therapist Insight',
+    subtext:
+      'For some, knowing a bit about their therapist is the spice that makes our journey more flavorful and helps build trust along the way.',
+    buttonText: 'Peek Into My Story',
+    buttonLink: '/my-story',
+  },
+  {
+    image: LearnMore,
+    header: 'Questions, The Essential Spice for Clarity',
+    subtext:
+      'It’s common to have more questions than answers when it comes to our healing journeys. Check out some key details that you might find helpful.',
+    buttonText: 'Learn More',
+    buttonLink: '/faq',
+  },
+];
---
+++ toasted-sesame-therapy/src/components/Home/constants/reasons.js
@@ -0,0 +1,26 @@
+export const reasons = [
+  {
+    title: 'Foreclosure Excess Funds',
+    content:
+      'Facing foreclosure can be challenging. However, there\'s often a silver lining called excess funds or overages that many aren\'t aware of. When a home is sold in foreclosure, the selling price can sometimes exceed the owed amount. We specialize in assisting homeowners with foreclosure excess funds retrieval, ensuring you reclaim any funds you\'re rightfully entitled to.',
+    direction: 'right',
+  },
+  {
+    title: 'Land Wholesaling',
+    content:
+      'Land is a unique asset that can sometimes be challenging to sell on traditional platforms. Our expertise in land wholesaling ensures you get the best value for your property. We connect sellers with a network of eager buyers, streamlining the process and making sure you receive a fair deal.',
+    direction: 'left',
+  },
+  {
+    title: 'Real Estate Solutions',
+    content:
+      'Our team of experienced professionals is dedicated to guiding you through every step, making sure you achieve your property goals.',
+    direction: 'right',
+  },
+  {
+    title: 'Your Success, Our Commitment',
+    content:
+      'In the dynamic world of real estate, having a reliable partner can make a world of difference. At our core, we believe in putting our clients\' interests first. We\'re committed to ensuring the success of our clients in every venture, and we work tirelessly to provide the resources, expertise, and support necessary to excel.',
+    direction: 'right',
+  },
+];
