cf99934766da338e9c966cbe80fe0b6142d31d81
Kato <zzeroyzz@gmail.com>
2023-12-02 20:29:23 -0500

    feat: updated eslint

--- toasted-sesame-therapy/.eslintrc.js
+++ toasted-sesame-therapy/.eslintrc.js
@@ -1,33 +1,37 @@
 module.exports = {
     "env": {
-        "node": true,
-        "browser": true,
-        "es2021": true
+      "node": true,
+      "browser": true,
+      "es2021": true
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+      "eslint:recommended",
+      "plugin:react/recommended"
     ],
     "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
+      {
+        "env": {
+          "node": true
+        },
+        "files": [
+          ".eslintrc.{js,cjs}"
+        ],
+        "parserOptions": {
+          "sourceType": "script"
         }
+      }
     ],
     "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+      "ecmaVersion": "latest",
+      "sourceType": "module"
     },
     "plugins": [
-        "react"
+      "react"
     ],
     "rules": {
+      // Add specific rule configurations here
+      "react/prop-types": "off", // Disable prop-types validation
+      "react-hooks/exhaustive-deps": "off", // Disable exhaustive-deps rule
+      "react/no-unescaped-entities": "off" // Disable unescaped-entities rule
     }
-}
+  };
