d628ffe0c972f72d88a0f7ae12a03a9219e79854
Kato <zzeroyzz@gmail.com>
2023-12-03 10:41:43 -0500

    feat: updated mailchimp server

--- toasted-sesame-therapy/src/components/ContactUs/hooks/useMailchimp.js
+++ toasted-sesame-therapy/src/components/ContactUs/hooks/useMailchimp.js
@@ -20,18 +20,14 @@ export const useMailchimp = () => {
       });
 
       if (!response.ok) {
-        // If the server responds with a status code that indicates an error, throw an error
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      // If the response is ok, parse the JSON body of the response
       const result = await response.json();
       setData(result);
     } catch (err) {
-      // If the fetch itself fails (e.g., due to network error), catch the error and set it
       setError(err);
     } finally {
-      // In both cases, we want to set isLoading to false after the operation is complete
       setIsLoading(false);
     }
   };
