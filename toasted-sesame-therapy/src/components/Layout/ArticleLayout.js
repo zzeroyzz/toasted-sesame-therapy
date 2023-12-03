6e2e36abe6f09e76ca25ac425c31cc99c01be7bf
Kato <zzeroyzz@gmail.com>
2023-12-02 19:34:09 -0500

    feat: updated layout

---
+++ toasted-sesame-therapy/src/components/Layout/ArticleLayout.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
+
+const MotionBox = motion(Box);
+
+const ArticleLayout = ({ children, ...props }) => {
+  const boxShadow = useColorModeValue('xl', 'dark-lg');
+  const fadeIn = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1, transition: { duration: 1.2 } },
+  };
+
+  return (
+    <MotionBox
+      pt={150}
+      pb={100}
+      px={10}
+      backgroundColor="#e8dfeb"
+      color="gray.700"
+      maxWidth="1000px"
+      textAlign="center"
+      margin="auto"
+      borderRadius="lg"
+      boxShadow={boxShadow}
+      variants={fadeIn}
+      initial="initial"
+      animate="animate"
+      {...props}
+    >
+      <Flex
+        minH="100vh"
+        direction="column"
+        align="center"
+        justify="start"
+      >
+ {children}
+ </Flex>
+    </MotionBox>
+  );
+};
+
+export default ArticleLayout;
