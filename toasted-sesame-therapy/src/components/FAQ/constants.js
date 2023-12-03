8dddaeac1d9ad49bedc0e1f5040af799f2b7a965
Kato <zzeroyzz@gmail.com>
2023-12-02 19:30:10 -0500

    feat: created faq component

---
+++ toasted-sesame-therapy/src/components/FAQ/constants.js
@@ -0,0 +1,40 @@
+export const faqData = [
+  {
+    question: "What is the cost per session, and do you accept insurance?",
+    answer: "Sessions are $125 for 50 mins or $65 for 30 minutes. I do not work with insurance companies, but can provide you with documentation for potential reimbursement."
+  },
+  {
+    question: "Are sessions virtual/online or in-person?",
+    answer: "I provide virtual telehealth sessions, but do not have an in-person location at this time."
+  },
+  {
+    question: "How long is each therapy session?",
+    answer: "Sessions are scheduled for 50 minutes, however, we can always discuss shortening or extending the session based on needs and my schedule availability."
+  },
+  {
+    question: "How often are sessions recommended?",
+    answer: "The frequency of sessions depends on individual needs and goals. However, weekly therapy is not always financially accessible, so we can discuss what therapy looks like at different paces."
+  },
+  {
+    question: "How can I schedule an appointment?",
+    answer: "You can request an appointment through the Client Portal. From there, click 'Request Appointment' to choose a time for the consultation. Feel free to email me if you prefer to schedule directly."
+  },
+  {
+    question: "What if I need to cancel or reschedule a session?",
+    answer: "Please notify me at least 48 hours in advance to avoid a cancellation fee."
+  },
+  {
+    question: "Who are my Ideal Clients?",
+    answer: "My ideal clients are people who are BIPOC, HNAAPI, SWANA, Transracial or Transnational Adoptees, Queer And Trans Folks, Neurodivergent Folks, Nurses, Therapists, Artists, Gamers, People with Chronic Pain/Illness, and Nonmonagamous Folks."
+  },
+  {
+    question: "What are your credentials and background?",
+    answer: `I am a Licensed Professional Counselor (LPC013996) in the State of Georgia.
+    I have a Master of Science in Clinical Mental Health Counseling from Mercer University Atlanta and my Bachelor of Science in Psychology from Kennesaw State University.
+    I've worked in both higher education and private group practice. I spent a year at SCAD, assisting artists at the student counseling services center, and then worked for three years in private practice, focusing on supporting gender, sexuality, and relationship structure minorities.
+    I've finished 60 hours of specialized training in Integrative Somatic Trauma Therapy at The Embody Lab. Currently, I'm in the process of completing the National Adoption Competency Mental Health Training offered by the Center for Adoption Support and Education.
+    `
+  },
+];
+
+export default faqData;
