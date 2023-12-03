dd0e8091d29d4decd520e59105436a367872dc5d
Kato <zzeroyzz@gmail.com>
2023-12-02 19:37:03 -0500

    feat: creatd therapeutic approach component

---
+++ toasted-sesame-therapy/src/components/TherapeuticApproachGuide/constants.js
@@ -0,0 +1,35 @@
+
+import Consent from '../../assets/Consent.jpg';
+import antioppression from '../../assets/antioppression.jpg';
+import antihierarchy from '../../assets/antihierarchy.jpg';
+import Relational from '../../assets/Relational.jpg';
+
+export const therapeuticApproachData = [
+  {
+    title: 'Relational',
+    img: Relational,
+    content:
+    `The therapeutic relationship that we build is one of the strongest predictors to our success together. The foundations of our rapport will be centered around boundaries, communication, and transparency. I invite you to let me know what is and is not working, to say no, and to disagree with me. `
+  },
+  {
+    title: 'Consent',
+    img: Consent,
+    content:
+    `I approach therapy from a model of informed consent. I check in frequently around continuing your work with me, setting and following intentions, meeting the needs of each week, and any specific therapeutic interventions you would like to fold into session.`
+  },
+  {
+    title: 'Anti-Oppression',
+    img: antioppression,
+    content:
+    `Working from an anti-oppression lens allows us to acknowledge the impact of systemic injustice as part of our fatigue, anxiety, and mental health struggles. In our work, we can focus on shifting blame away from self, to structures of oppression. `
+  },
+  {
+    title: 'Anti-Hierarchical',
+    img: antihierarchy,
+    content:
+    `I place great importance on building a non-hierarchical collaborative relationship between counselor and client. Our space would emphasize transparency, mutuality, empowerment, and choice.`
+  },
+
+];
+
+export default therapeuticApproachData;
