2be96e1d0468fa182a4617513b3b2a8470597299
Kato <zzeroyzz@gmail.com>
2023-12-02 19:35:16 -0500

    feat: updated logo component for layout

---
+++ toasted-sesame-therapy/src/components/Layout/ToastedSesameLogo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Image} from '@chakra-ui/react';
+
+const ToastedSesameLogo = () => (
+<Image src='/ToastedSesameLogo.png' alt="Pothos Logo White" width={100} borderRadius="50%" />);
+
+
+
+export {ToastedSesameLogo};
