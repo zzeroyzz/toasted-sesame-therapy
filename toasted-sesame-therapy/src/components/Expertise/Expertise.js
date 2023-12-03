123ca150d0d7547a739ca72290e9931eda69a58a
Kato <zzeroyzz@gmail.com>
2023-12-02 19:28:31 -0500

    feat: updated expertise component

---
+++ toasted-sesame-therapy/src/components/Expertise/Expertise.js
@@ -0,0 +1,124 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Box,
+  Text,
+  Heading,
+  VStack,
+  Link,
+  Image,
+  Divider,
+  Flex,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import {motion} from 'framer-motion';
+import {expertiseData, subtitleText} from './constants';
+import ArticleLayout from '../Layout/ArticleLayout';
+import FlowerAnimations from '../MyStory/FlowerAnimations/FlowerAnimations';
+
+const MotionBox = motion(Box);
+const MotionImage = motion(Image);
+const MotionText = motion(Text);
+const MotionVStack = motion(VStack);
+
+const fadeIn = {
+  initial: {opacity: 0},
+  animate: {opacity: 1, transition: {duration: 1.2}},
+};
+
+const Expertise = () => {
+const dividerColor = useColorModeValue('gray.400', 'gray.600');
+
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollFadeIn = {
+    hidden: {opacity: 0},
+    show: (i) => ({
+      opacity: 1,
+      transition: {delay: i * 0.2, duration: 1},
+      y: 0,
+    }),
+    initial: {opacity: 0, y: 100},
+  };
+
+  return (
+    <ArticleLayout
+      variants={fadeIn}
+      initial="initial"
+      animate="animate"
+    >
+      <FlowerAnimations/>
+      <MotionVStack spacing={8} alignItems="center">
+        <Heading as="h1" size="2xl">
+          Explore My Expertise
+
+        </Heading>
+        <Text fontSize="xl" textAlign="center" w="80%">
+          {subtitleText}
+        </Text>
+
+        <MotionVStack spacing={10} alignItems="start" w="80%">
+          {expertiseData.map((section, index) => (
+            <MotionBox key={section.title} variants={fadeIn} width="100%">
+              <Flex justifyContent="center" width="100%">
+                <MotionImage
+                  w={['100%', '85%', '75%']}
+                  src={section.img}
+                  alt={section.title}
+                  boxShadow="lg"
+                  mb={4}
+                  borderRadius="md"
+                  variants={scrollFadeIn}
+                  initial="initial"
+                  custom={index}
+                  animate={scrollY > index * 150 ? 'show' : 'hidden'}
+                />
+              </Flex>
+              <MotionText fontWeight="bold" fontSize="2xl" textAlign="left" paddingBottom="1rem">
+                {section.title}
+              </MotionText>
+              <MotionText textAlign="justify" fontSize="xl">{section.content}</MotionText>
+            </MotionBox>
+          ))}
+        </MotionVStack>
+
+        <Divider borderColor={dividerColor} w="80%" my={5} />
+        <Link
+        href="/contact"
+        _hover={{textDecoration: 'none', transform: 'scale(1.05)'}}
+        transition="transform 0.2s ease-in-out"
+      >
+        <Text fontSize="lg" mt={5} textAlign="center" w="100%">
+        Have more questions? 🫰🏻
+        </Text>
+      </Link>
+
+        <Divider borderColor={dividerColor} w="80%" my={5} />
+        <Flex justify="space-between" width="80%">
+          <Text fontSize="sm" color="gray.500">
+            © 2023 by Kay
+          </Text>
+          <Link
+            href="#"
+            fontSize="sm"
+            color="blue.500"
+            _hover={{textDecoration: 'none', transform: 'scale(1.05)'}}
+            transition="transform 0.2s ease-in-out"
+          >
+            Back to top
+          </Link>
+        </Flex>
+      </MotionVStack>
+    </ArticleLayout>
+  );
+};
+
+export default Expertise;
---
+++ toasted-sesame-therapy/src/components/Expertise/constants.js
@@ -0,0 +1,119 @@
+import Identity from '../../assets/Identity.jpg';
+import ContinualGrowth from '../../assets/ContinualGrowth.jpg';
+import Mindbody from '../../assets/Mindbody.jpg';
+import CoreValues from '../../assets/CoreValues.jpg';
+
+export const expertiseData = [
+  {
+    title: 'Core Values in Trauma Therapy',
+    img: CoreValues,
+    content:
+    `Trauma, woven into our experiences, impacts our mental, emotional, and physical well-being. My focus on trauma therapy aims to create a secure, supportive space for you to regain agency from its effects. I value consent, communication, creativity, empowerment, agency, and resourcing from your own wonderful strengths and qualities.These values are the foundations to deeply respecting and building understanding for each person’s unique experiences of trauma. `
+  },
+  {
+    title: 'The Mind-Body Connection',
+    img: Mindbody,
+    content:
+    `Somatic Experiencing is a therapeutic approach that recognizes the profound connection between mind and body. By engaging with sensations and movements, we unlock additional pathways for healing, allowing you to connect with your body in a way that complements traditional talk therapy`
+  },
+  {
+    title: 'Continual Growth in Somatic Healing Practices',
+    img: ContinualGrowth,
+    content:
+    `My professional training encompasses insights from both Somatic Experiencing and trauma education, including Internal Family Systems (IFS), Somatic Archeology, Sensorimotor Psychotherapy, and Polyvagal Theory. If you feel disconnected from your bodily experience, tend to think more than feel, experience chronic pain or inflammation in connection with emotions, or have resonated with mind-body healing - we can explore if Somatic Experience would be supportive for you.`
+  },
+  {
+    title: 'Identity as a Therapeutic Tool',
+    img: Identity,
+    content:
+    `As a therapist, my commitment to an affirming space is rooted in my identities as a Korean American who is queer, neurodivergent, and living with CPTSD. These aspects of my identity are not only integral to who I am but also serve as a lens through which I approach therapeutic work. Understanding firsthand the intersections of marginalized identities allows me to navigate the nuanced experiences of those I work with, making space for genuine connection and empathy. I wish to bring authenticity to the therapeutic relationship, creating an environment where individuals can explore their own identities, relationships, and paths to healing with openness and understanding. `
+  }
+];
+
+export const buyerProcessData = [
+  {
+    title: '1. Discover',
+    content: 'Ask us about our curated list of available properties.',
+  },
+  {
+    title: '2. Choose',
+    content: 'Select a parcel of land that aligns with your goals.',
+  },
+  {
+    title: '3. Secure the Deal',
+    content: 'Sign the purchase agreement to lock in your land.',
+  },
+  {
+    title: '4. Title and Deed',
+    content:
+      'We work diligently to ensure a seamless title transfer process. Our assessment fee, which covers the expenses incurred during the evaluation of the property, is managed directly by the title company. This way, you can be confident that all financial matters are handled transparently and professionally, free from any complications.',
+  },
+  {
+    title: '5. Finalize the Purchase',
+    content: 'Close the deal and begin your land ownership journey.',
+  },
+];
+
+export const subtitleText = `Embarking on your healing journey is a courageous step toward personal growth and well-being. I invite you to explore my specialized approach, particularly in trauma and somatic therapy. These aren't just therapeutic techniques; they're compasses guiding you through the intricate terrain of your healing process.`;
+
+export const sellerProcessData = [
+  {
+    title: '1. Initial Consultation',
+    content: 'Understand your needs and expectations.',
+  },
+  {
+    title: '2. Property Evaluation',
+    content:
+      'Check for qualifications including free and clear land, ensuring it\'s not in wetland or flood zones.',
+  },
+  {
+    title: '3. Offer Presentation',
+    content:
+      'If the land fits our criteria, we present you with a fair cash offer.',
+  },
+  {
+    title: '4. Contract Signing',
+    content: 'Agree on terms and sign the sales agreement.',
+  },
+  {
+    title: '5. Title and Deed',
+    content:
+      'We handle all title work to ensure it\'s free of any liens or encumbrances.',
+  },
+  {
+    title: '6. Close the Sale',
+    content: 'Swift closings so you can have cash in hand without any hassles.',
+  },
+];
+
+export const landProcessData = [
+  {
+    title: '1. Initial Consultation',
+    content: 'Understand your needs and expectations.',
+  },
+  {
+    title: '2. Property Evaluation',
+    content:
+      'Check for qualifications including free and clear land, ensuring it\'s not in wetland or flood zones.',
+  },
+  {
+    title: '3. Offer Presentation',
+    content:
+      'If the land fits our criteria, we present you with a fair cash offer.',
+  },
+  {
+    title: '4. Contract Signing',
+    content: 'Agree on terms and sign the sales agreement.',
+  },
+  {
+    title: '5. Title and Deed',
+    content:
+      'We manage all aspects of the title work, ensuring it\'s devoid of any liens or encumbrances. Our assessment fee, vital for evaluating the property\'s suitability, is professionally processed by the title company, ensuring transparency and minimizing any potential complications for our clients.',
+  },
+  {
+    title: '6. Close the Sale',
+    content: 'Swift closings so you can have cash in hand without any hassles.',
+  },
+];
+
+export default expertiseData;
